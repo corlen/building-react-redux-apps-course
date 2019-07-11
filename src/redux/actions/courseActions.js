513735409769f107f6155edf74df0db71a99961c
Maximiliano Santiago <maximiliano.santiago@gmail.com>
2019-07-11 19:08:48 -0400

    Fix some minor lint warnings

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -34,7 +34,7 @@ export function loadCourses() {
 }
 
 export function saveCourses(course) {
-  return function(dispatch, getState) {
+  return function(dispatch) {
     dispatch(beginApiCall());
     return courseApi
       .saveCourse(course)
